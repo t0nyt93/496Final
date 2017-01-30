45c786f2daefc311359e5ffb2b0ac4243704c0d4
Tony <tonytyrrell93@gmail.com>
2017-01-30 12:56:52 -0800

    Changing landing page

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
 from flask import Flask
 import time, datetime
 import webapp2
-app = Flask(__name__)
+#app = Flask(__name__)
 
-app.config['DEBUG'] = True
+#app.config['DEBUG'] = True
 
 # Note: We don't need to call run() since our application is embedded within
 # the App Engine WSGI application server.
+class HelloWebapp2(webapp2.RequestHandler):
+    def get(self):
+        self.response.write('Hello, webapp2!')
 
+app = webapp2.WSGIApplication([
+    ('/', HelloWebapp2),
+], debug=True)
 
+def main():
+    app.run()
+
+if __name__ == '__main__':
+    main()
+'''
 @app.route('/')
 def hello():
     var = time.time()
@@ -21,3 +33,4 @@ def hello():
 def page_not_found(e):
     """Return a custom 404 error."""
     return 'Sorry, nothing at this URL.', 404
+'''
