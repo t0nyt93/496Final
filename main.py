403ad06aea5729e354b5ea18100b3e117799c264
Tony <tonytyrrell93@gmail.com>
2017-01-31 01:09:26 -0800

    Fixes server error and enforces PEP8

--- main.py
+++ main.py
@@ -7,25 +7,31 @@ import webapp2
 
 # Note: We don't need to call run() since our application is embedded within
 # the App Engine WSGI application server.
+
+
 class WelcomeHandler(webapp2.RequestHandler):
     def get(self):
         self.respone.headers['Content-Type'] = 'text/plain'
         self.response.write('Welcome to the Home URL of my CS496 Website. !')
 
+
 class BookHandler(webapp2.RequestHandler):
     def get(self):
         self.response.write('Hello, Book Handler!')
+
 
 class CustomerHandler(webapp2.RequestHandler):
     def get(self):
         self.response.write('Hello, Customer Handler!')
 
+
 def handle_404(request, response, exception):
-    response.write( ' The URL you requested isn\'t valid in this site!' )
+    response.write(' The URL you requested isn\'t valid in this site!')
     response.set_status(404)
 
+
 def handle_500(request, response, exception):
-    response.write( exception )
+    response.write(exception)
     response.set_status(500)
 
 
@@ -33,28 +39,18 @@ app = webapp2.WSGIApplication([
     ('/', WelcomeHandler),
 ], debug=True)
 
-app.router.add( (r'/books',BooksHandler) )
-app.router.add( (r'/customer',CustomerHandler) )
-
 app.error_handlers[404] = handle_404
 app.error_handlers[500] = handle_500
+
+app.router.add((r'/books', BookHandler))
+app.router.add((r'/customer', CustomerHandler))
+
+
+
 
 def main():
     app.run()
 
 if __name__ == '__main__':
     main()
-'''
-@app.route('/')
-def hello():
-    var = time.time()
-    t = datetime.datetime.fromtimestamp(var).strftime('%Y-%m-%d %H:%M:%S')
 
-    """Return a friendly HTTP greeting."""
-    return t
-
-@app.errorhandler(404)
-def page_not_found(e):
-    """Return a custom 404 error."""
-    return 'Sorry, nothing at this URL.', 404
-'''
