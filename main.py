1cf407020572f5f94f7f64cbe9e72a6c3a69c24c
Tony <tonytyrrell93@gmail.com>
2017-01-31 22:04:15 -0800

    Changes Regex routing

--- main.py
+++ main.py
@@ -78,8 +78,8 @@ app = webapp2.WSGIApplication([
 app.error_handlers[404] = handle_404
 app.error_handlers[500] = handle_500
 
-app.router.add((r'/books/(\d+)', BookHandler))
-app.router.add((r'/customers/(\d+)', CustomerHandler))
+app.router.add((r'/books/(.*)', BookHandler))
+app.router.add((r'/customers/(\.*)', CustomerHandler))
 
 
 
