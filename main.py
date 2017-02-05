a4a9e2f4dce1647fdad8637bbe56000d991595d2
Tony <tonytyrrell93@gmail.com>
2017-02-04 19:15:40 -0800

    Completes REST API, lots of room for refactoring.

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-from flask import Flask
-import time, datetime
-
 import json
 import webapp2
 import os
+import cgi
 from google.appengine.ext import ndb
-
+from cStringIO import StringIO
 
 # RESTful permissions
 PERMISSION_ANYONE = 'anyone'
@@ -13,31 +11,54 @@ PERMISSION_LOGGED_IN_USER = 'logged_in_user'
 PERMISSION_OWNER_USER = 'owner_user'
 PERMISSION_ADMIN = 'admin'
 
+#So my limited understanding leads me to belive that these keys are solely available
+#in the context of the Process Memory. Therefore if we've restarted the server, we
+#lost all of the keys that we'd previously created, granted we can the objects with query...
+#
+#HALLELUJAH
+#If you're interested the server could have a startup task that got all entities, and then just did
+# for entity in books:
+#   book_keys.append(entity.key())
+#I feel like you've already got the overhead of the Query though, so maybe you're just saving on process
+#memory at that point..
+
 customer_keys = []
 book_keys = []
+
+objects = []
 """
 Create Database Models
 """
 
 
-class customerModel(ndb.Model):
-    id = ndb.IntegerProperty
-    title = ndb.StringProperty
-    isbn = ndb.IntegerProperty
-    genre = [ ndb.StringProperty() ]
-    author = ndb.StringProperty
-
-
 class bookModel(ndb.Model):
-    id = ndb.IntegerProperty
-    name = ndb.StringProperty
-    balance = ndb.IntegerProperty
-    checked_out = [ ndb.StringProperty ]
+    id = ndb.IntegerProperty()
+    title = ndb.StringProperty()
+    isbn = ndb.StringProperty()
+    genre = ndb.StringProperty(repeated=True)
+    author = ndb.StringProperty()
+    checkedIn = ndb.BooleanProperty()
+
+class customerModel(ndb.Model):
+    id = ndb.IntegerProperty()
+    name = ndb.StringProperty()
+    balance = ndb.StringProperty()
+    checked_out = ndb.StringProperty(repeated=True)
 
 
 class WelcomeHandler(webapp2.RequestHandler):
     def get(self):
         self.response.write('Hello, CS496!')
+
+    def delete(self):
+        b = bookModel.query()
+        c = customerModel.query()
+
+        for book in b:
+            book.key.delete()
+        for customer in c:
+            customer.key.delete()
+        self.response.write("Successfully deleted all books and customers.")
 
 """
 Customer Handler
@@ -45,86 +66,321 @@ Customer Handler
 Provides a REST api for creating, updating, and deleting book objects
 """
 
-class BookHandler(webapp2.RequestHandler):
-    def get(self, book_id):
-        #Ok so let's just parse the book_id and then figure out what to return?
-        url = book_id.split("/")
 
-        #/books/authors/:name
-        if (url[0] == 'author' ) and (len(url) == 2):
-            #Get all of the books where the authors name is url[1]
-            self.response.write("Viewing books by author, desired %s" % url[1])
-        #/books/genre/:type
-        elif (url[0] == 'genre') and (len(url) == 2):
-            # Get all of the books where the authors name is url[1]
-            self.response.write("Viewing books by genre, desired %s" % url[1])
-        #/books/
-        elif (url[0] == "") and (len(url) == 1):
-            #Get all of the books
-            self.response.write("Viewing all books ")
-        #/books/:id
-        elif (url[0].isdigit() ) and (len(url) == 1):
-            #Lookup the books ID and return the json.
-            #/books/:id
-            self.response.write('Book id is %s' % book_id)
-"""
-    def put(self, arg):
-        #Put to /books/
-        if arg == "":
+class BookListHandler(webapp2.RequestHandler):
+    b = bookModel.query()
+    book_id = b.count() + 1
+    """
+    RESTful GET @ /books/
+    """
+    def get(self, args):
+        output = []
+        path_info = parse_url(args)
+        path_len = path_info[0]
+        path = path_info[1]
 
-        #PUT to /books/id
-        elif arg.isDigit():
-            #Get the book with the right ID and change it's values
+        #/books
+        if args == "" or args == "/":
+            self.response.headers['Content-Type'] = 'application/json'
+            if self.request.get("checkedIn"):
+                flag = self.request.get("checkedIn").capitalize()
+                if flag == "True":
+                    books = self.b.filter(bookModel.checkedIn == True)
+                else:
+                    books = self.b.filter(bookModel.checkedIn == False)
 
-    def delete(self, arg):
-        # Put to /books/
-        if arg == "":
+                for book in books:
+                    output.append(json.dumps(book.to_dict()))
+            else:
+                for book in self.b:
+                    output.append(json.dumps(book.to_dict()))
 
-        # PUT to /books/id
-        elif arg.isDigit():
+        elif path_len == 2 and path[1].isdigit():
+            desired_book = self.b.filter(bookModel.id == int(path[1]))
+            for x in desired_book:
+                output.append(json.dumps(x.to_dict()))
 
-    # Get the book with the right ID and change it's values
-    def post(self):
-        #Create a new record with the given body
+        self.response.headers['Content-Type'] = 'application/json'
+        self.response.write(output)
 
-    def patch(self,arg):
-        # Put to /books/
-        if arg == "":
+    def post(self, args):
+        if args == "" or args == "/":
+            try:
+                checked_flag = False
+                if self.request.get('checkedIn') == ("True" or "true"):
+                    checked_flag = True
 
-        # Update the book with the corresponding ID
-        elif arg.isDigit():"""
+                new_book = bookModel(
+                    id=self.book_id,
+                    title=self.request.get('title'),
+                    isbn=self.request.get('isbn'),
+                    genre=self.request.get_all('genre'),
+                    author=self.request.get('author'),
+                    checkedIn=checked_flag
+                )
+                #Send the new entry to the Datastore
+                new_book.put()
+                self.response.write(json.dumps(new_book.to_dict()))
+            except Exception as e:
+                self.response.write("Seems we encountered an error during object creation... \n %s" %e)
+        else:
+            self.response.write("Not posting to the right URL! Try /customers")
+
+    def delete(self, args):
+        path_info = parse_url( args )
+        path_len = path_info[0]
+        path = path_info[1]
+        #Lets delete a customer by id
+        if path_len == 2 and path[1].isdigit():
+            book_by_id = self.b.filter(bookModel.id == int(path[1]))
+            for x in book_by_id:
+                x.key.delete()
+
+        elif args == "" or args == "/":
+            for book in self.b:
+                book.key.delete()
+
+        self.response.write("")
+
+    def put(self, args):
+        path_info = parse_url(args)
+        path_len = path_info[0]
+        path = path_info[1]
+
+        #Lets delete a customer by id
+        if path_len == 2 and path[1].isdigit():
+            book_by_id = self.b.filter(bookModel.id == int(path[1]))
+            for x in book_by_id:
+                #Replace the books contents
+                if self.request.get('title'):
+                    x.title = self.request.get('title')
+                else:
+                    x.title = ""
+                if self.request.get('author'):
+                    x.author = self.request.get('author')
+                else:
+                    x.author = ""
+                if self.request.get('isbn'):
+                    x.isbn = self.request.get('isbn')
+                else:
+                    x.isbn = ""
+                if self.request.get('genre'):
+                    x.genre = self.request.get_all('genre')
+                else:
+                    x.genre = []
+                if self.request.get('id'):
+                    x.id = int(self.request.get('id'))
+                if self.request.get('checkedIn'):
+                    x.checkedIn = bool(self.request.get('checkedIn'))
+                else:
+                    x.checkedIn = True
+                x.put()
+                self.response.write(json.dumps(x.to_dict()))
+
+    def patch(self, args):
+        path_info = parse_url(args)
+        path_len = path_info[0]
+        path = path_info[1]
+        filepointer = StringIO(self.request.body)
+        form = cgi.FieldStorage(
+            fp = filepointer,
+            headers = self.request.headers,
+            environ= { 'REQUEST_METHOD' : 'PATCH',
+                       'CONTENT_TYPE' : self.request.headers['content-type']
+                    }
+            )
+        title = form.getfirst("title","")
+        author = form.getfirst("author","")
+        isbn = form.getfirst('isbn', "")
+        genres = form.getlist('genre')
+        bookId = form.getfirst('id', "")
+        checkFlag = form.getfirst('checkedIn', "")
+
+        #Lets delete a customer by id
+        if path_len == 2 and path[1].isdigit():
+            book = self.b.filter(bookModel.id == int(path[1])).get()
+            #Replace the books contents
+            if title:
+                book.title = title
+            if author:
+                book.author = author
+            if isbn:
+                book.isbn = isbn
+            if genres:
+                book.genre = genres
+            if bookId:
+                book.id = bookId
+            if checkFlag:
+                book.checkedIn = bool(checkFlag)
+            book.put()
+            self.response.write(json.dumps(book.to_dict()))
+
 """
 Customer Handler
 
 Provides a REST api for creating, updating, and deleting Customer objects
 """
-
-class CustomerHandler(webapp2.RequestHandler):
+class CustomerListHandler(webapp2.RequestHandler):
     c = customerModel.query()
-    next_id = len(c) + 1
+    next_id = c.count() + 1
 
-    def get(self, customer_id):
-        if customer_id.isDigit():
-            desired_customer = customerModel.query(customerModel.id == int(customer_id))
-            if desired_customer:
+    def get(self, args):
+        output = ""
+        #Checking a book in
+        path_info = parse_url(args)
+        path_len = path_info[0]
+        path = path_info[1]
+        #Get all of the customers
+        if args == "" or args == "/":
+            for customer in self.c:
+                output += json.dumps(customer.to_dict()) + "\n"
                 self.response.headers['Content-Type'] = 'application/json'
-                self.response.write( json.dumps(desired_customer.to_dict()))
-            else:
-                self.response.write("Could not find a customer with the ID %s, have you created one yet?" % customer_id)
+            if output == "":
+                output += "No customerModels were found :("
 
+        elif path_len == 2 and path[1].isdigit():
+            cust_by_id = self.c.filter(customerModel.id == int(path[1])).get()
+            self.response.headers['Content-Type'] = 'application/json'
+            output += json.dumps(cust_by_id.to_dict())
+            if output == "":
+                output += "Couldn\'t find customer %s!" % c_id
+
+        elif path_len == 4 and path[1].isdigit() and path[2] == "books":
+            cust_by_id = self.c.filter(customerModel.id == int(path[1])).get()
+            for x in cust_by_id.checked_out:
+                if x:
+                    b_id = x.split("/")
+                    if b_id[2].isdigit():
+                        self.response.headers['Content-Type'] = 'application/json'
+                        book = bookModel.query(bookModel.id == int(b_id[2])).get()
+                        output += json.dumps(book.to_dict())
         else:
-            self.response.write('Customer ID\'s consist of solely digits!')
+            output = "url not valid"
 
 
-    def post(self):
-        new_customer = customerModel()
-        new_customer.id = self.next_id
-        new_customer.name = self.request.get('name', "John Doe")
-        new_customer.balance = self.request.get('balance', "0")
-        new_customer.checked_out = self.request.get_all('checked_out', "")
-        cust_key = new_customer.put()
-        customer_keys.append(cust_key)
+        self.response.write(output)
 
+    def post(self, args):
+        if args == "" or args == "/":
+            try:
+                new_customer = customerModel(
+                    id=self.next_id,
+                    name=self.request.get('name'),
+                    balance=self.request.get('balance'),
+                    checked_out=self.request.get_all('checked_out')
+                )
+
+                cust_key = new_customer.put()
+                customer_keys.append( cust_key)
+                self.response.headers['Content-Type'] = 'application/json'
+                self.response.write(json.dumps(new_customer.to_dict()))
+            except Exception as e:
+                self.response.write("Seems we encountered an error during object creation... \n %s" %e)
+        else:
+            self.response.write("Not posting to the right URL! Try /customers")
+
+    def delete(self, args):
+        #Checking a book in
+        path_info = parse_url(args)
+        path_len = path_info[0]
+        path = path_info[1]
+        if path_len == 2 and path[1].isdigit():
+            cust_by_id = self.c.filter(customerModel.id == int(path[1]))
+            for x in cust_by_id:
+                x.key.delete()
+
+        # Check In a book
+        if path_len == 4 and path[1].isdigit() and path[3].isdigit() and path[2] == "books":
+            c_id = path[1]
+            b_id = path[3]
+
+            cust_by_id = self.c.filter(customerModel.id == int(c_id)).get()
+            book_by_id = bookModel.query(bookModel.id == int(b_id)).get()
+            cust_by_id.checked_out.remove("/books/" + b_id)
+            book_by_id.checkedIn = True
+            book_by_id.put()
+            cust_by_id.put()
+        if args == "" or args == "/":
+            for cust in self.c:
+                cust.key.delete()
+
+    #Replace contents at
+    def put(self, args):
+        path_info = parse_url(args)
+        path_len = path_info[0]
+        path = path_info[1]
+        # Lets delete a customer by id
+        if path_len == 4 and path[1].isdigit() and path[3].isdigit() and path[2] == "books":
+            c_id = path[1]
+            b_id = path[3]
+
+            cust_by_id = self.c.filter(customerModel.id == int(c_id)).get()
+            book_by_id = bookModel.query(bookModel.id == int(b_id)).get()
+            cust_by_id.checked_out.append("/books/" + b_id)
+            book_by_id.checkedIn = False
+
+            book_by_id.put()
+            cust_by_id.put()
+
+            self.response.headers['Content-Type'] = 'application/json'
+            self.response.status = 201
+            self.response.write(json.dumps(cust_by_id.to_dict()))
+
+        #Lets update a customer by id
+        if path_len == 2 and path[1].isdigit():
+            cust_by_id = self.c.filter(customerModel.id == int(path[1]))
+            for x in cust_by_id:
+                #Replace the books contents
+                if self.request.get('name'):
+                    x.name = self.request.get('name')
+                else:
+                    x.name = ""
+                if self.request.get('balance'):
+                    x.balance = self.request.get('balance')
+                else:
+                    x.balance = ""
+                if self.request.get('checked_out'):
+                    x.checked_out = self.request.get_all('checked_out')
+                else:
+                    x.checked_out = []
+                if self.request.get('id'):
+                    x.id = int(self.request.get('id'))
+
+                x.put()
+                self.response.write(json.dumps(x.to_dict()))
+
+    def patch(self, args):
+        path_info = parse_url(args)
+        path_len = path_info[0]
+        path = path_info[1]
+        filepointer = StringIO(self.request.body)
+        form = cgi.FieldStorage(
+            fp = filepointer,
+            headers = self.request.headers,
+            environ= { 'REQUEST_METHOD' : 'PATCH',
+                       'CONTENT_TYPE' : self.request.headers['content-type']
+                    }
+            )
+        name = form.getfirst("name","")
+        balance = form.getfirst("balance","")
+        cId = form.getfirst('id', "")
+        checkOut = form.getlist('checked_out')
+
+        #Lets delete a customer by id
+        if path_len == 2 and path[1].isdigit():
+            cust = self.c.filter(customerModel.id == int(path[1])).get()
+            #Replace the books contents
+            if name:
+                cust.name = name
+            if balance:
+                cust.balance = balance
+            if cId:
+                cust.id = cId
+            if checkOut:
+                cust.checked_out = checkOut
+
+            cust.put()
+            self.response.write(json.dumps(cust.to_dict()))
 
 def handle_404(request, response, exception):
     response.write(' The URL you requested isn\'t valid in this site!')
@@ -136,6 +392,9 @@ def handle_500(request, response, exception):
     response.set_status(500)
 
 
+allowed_methods = webapp2.WSGIApplication.allowed_methods
+new_allowed_methods = allowed_methods.union(('PATCH',))
+webapp2.WSGIApplication.allowed_methods = new_allowed_methods
 app = webapp2.WSGIApplication([
     ('/', WelcomeHandler),
 ], debug=True)
@@ -143,13 +402,12 @@ app = webapp2.WSGIApplication([
 app.error_handlers[404] = handle_404
 app.error_handlers[500] = handle_500
 
-app.router.add((r'/books/(.*)', BookHandler))
-app.router.add((r'/customers/(.*)', CustomerHandler))
+app.router.add((r'/books(.*)', BookListHandler))
+app.router.add((r'/customers(.*)', CustomerListHandler))
 
-
-
-
-
+def parse_url( url ):
+    x = url.split("/")
+    return len(x),x
 
 
 def main():
@@ -157,4 +415,6 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
 
